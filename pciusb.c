DrakXtools/ldetect

commit 954372fc6555e0e2c546469001fc1a4ca03bd8ae

(pciusb_find_modules) handle pcitable without description field
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/ldetect/trunk@225445 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

pciusb.c
@@ -137,6 +137,8 @@ extern int pciusb_find_modules(struct pciusb_entries *entries, const char *fpciu
 			if (!p) { // only calc text & module if not already done
 				p = buf + offset + 1;
 				q = strchr(p, '\t');
+                    if (!q) // no description field?
+                         q = strchr(p, '\0') - 1;
 			}
 			if (strncmp(p, "unknown", q-p-1)) {
 				ifree(e->module);

commit 8f1e590eee0d1791775dd72eaff55a1b79f825d2

do not use random string as device description
broken since commit r225445 from 2007-08-06: (pciusb_find_modules) handle pcitable without description field

Broken output was the following:
Mouse:USB|Generic 2 Button Mouse: base.frm"
 [Human Interface Device|Boot Interface Subclass|Mouse] (vendor:0603 device:6871)

pciusb.c
@@ -205,7 +205,7 @@ extern int pciusb_find_modules(struct pciusb_entries *entries, const char *fpciu
 				e->module = strndup(p,q-p-1);
 			}
 			/* special case for buggy 0x0 usb entry */
-			if (descr_lookup == LOAD && 2 < strlen(q+2) && vendor != 0 && device != 0 && e->class_id != 0x90000d) { /* Hub class */
+			if (descr_lookup == LOAD && strlen(q) > 1 && 2 < strlen(q+2) && vendor != 0 && device != 0 && e->class_id != 0x90000d) { /* Hub class */
 				ifree(e->text); /* usb.c set it so that we display something when usbtable doesn't refer that hw*/
 				e->text = strndup(q+2, strlen(q)-4);
 			}